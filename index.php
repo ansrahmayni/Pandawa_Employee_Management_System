030b774c6df7098b781300c6bffd6ab77ee97107
ansrahmayni <ansrahmayni@gmail.com>
2024-09-25 09:32:31 +0700

    membuat home page

--- index.php
+++ index.php
@@ -2,41 +2,75 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@400;700&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>Pandawa</h1>
-    <hr>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur corrupti dolorem repellat pariatur nulla ipsa voluptates, optio ab, explicabo odit exercitationem nemo! Corporis aliquid similique ad, officiis ut cupiditate repudiandae.</p>
+    <?php include 'components/header.php'; ?>
 
+    <h1>Pandawa</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur corrupti dolorem repellat pariatur nulla ipsa voluptates, optio ab, explicabo odit exercitationem nemo! Corporis aliquid similique ad, officiis ut cupiditate repudiandae.</p>
     <br><br>
-    <?php include 'show_employees.php'?>
+
+    <div class="btn">
+        <a href="pengisian_data.php" class="btn-form">Tambah Karyawan</a>
+        <a href="pengisian_data.php" class="btn-form">Tambah Project</a>
+    </div>
 </body>
 <style>
     body{
-        font-family: 'Montserrat';
+        font-family: 'Poppins';
+        background-image: url('asset/bg.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100%;
+        background-color: #fff;
     }
 
     h1{
         text-align: center;
         color: #000;
-        margin: 10px;
-        font-size: 45px;
+        -webkit-text-stroke: 0.5px #fff;
+        margin-top: 100px;
+        font-size: 50px;
         font-family: 'MuseoModerno';
-    }
-
-    hr{
-        width: 50%;
-        height: 3px;
-        background-color: #000;
     }
 
     p{
         text-align: center;
-    
+        padding: 0px 250px;
+        margin: 10px;
+        font-size: 18px;
+        color: #000 ;
+        -webkit-text-stroke: 0.5px #000;
+    }
+
+    .btn{
+        align-items: center;
+        text-align: center;
+    }
+
+    .btn-form{
+        background-color: rgba(253, 216, 53, 1);
+        color: #000;
+        padding: 25px 40px;
+        border: none;
+        margin: 0px 20px;
+        border-radius: 15px;
+        cursor: pointer;
+        box-shadow: 8px 8px 3px rgba(0, 0, 0, 0.5);
+        font-size: 17px;
+        text-decoration: none;
+    }
+
+    .btn-form:hover{
+        background-color: #FBC02D;
+        font-weight: bold;
+        display: ;
     }
 </style>
 </html>
